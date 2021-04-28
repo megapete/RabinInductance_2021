df35945fe21baa576cab488ea32fb05e8965d584
Peter C Huber <peter@hubersitech.com>
2021-04-28 18:41:16 -0400

    Initial Commit

---
+++ RabinInductance_2021/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RabinInductance_2021
+//
+//  Created by Peter Huber on 2021-04-28.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
